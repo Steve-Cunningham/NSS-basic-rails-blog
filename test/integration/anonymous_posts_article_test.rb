22fe3f538b96165262b277aee066a66e980d46dd
Steve-Cunningham <steven.j.cunningham@gmail.com>
2013-06-04 10:33:07 -0500

    Browse list of posts

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -4,13 +4,13 @@ class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
   test "view a list of articles" do
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
     Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
-
-    visit '/'
-    click_link "View All Posts"
-    assert_current_path '/posts'
+    visit '/posts'
+    assert_equal 2, Post.count
+    visit '/posts'
+    assert_include page.body, "All Posts"
     # assume we have a page variable
-    assert_include page.content, "First Post!!"
+    assert_include page.body, "First Post!!"
     #=> assert page.content.include? "First Post!!"
-    assert_include page.content, "Wow, it's been a while!"
+    assert_include page.body, "Wow, it's been a while!"
   end
 end
